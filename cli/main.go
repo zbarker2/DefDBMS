ffc0eecc17ed25ad2d9a69440834d064f2880c3f
zbarker2 <zave.panthers.5@gmail.com>
2024-02-11 16:58:23 -0500

    Goofing off with flags

--- cli/main.go
+++ cli/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Printf("yeet")
+	rootCmd := flag.NewFlagSet("test", flag.ExitOnError)
+	rootPrint := rootCmd.Bool("print", false, "print")
 
+	if len(os.Args) < 3 {
+		fmt.Println("Expected the 'printStuff' command")
+		os.Exit(1)
+	}
+	switch os.Args[2] {
+	case "-print":
+		rootCmd.Parse(os.Args[2:])
+		fmt.Println("printStuff", *rootPrint)
+	}
 }
